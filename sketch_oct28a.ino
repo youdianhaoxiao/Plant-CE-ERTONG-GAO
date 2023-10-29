1025ebaf240c938724e59543d581809039ef2945
youdianhaoxiao <146217421+youdianhaoxiao@users.noreply.github.com>
2023-10-29 12:03:16 +0000

    Update sketch_oct28a.ino

--- sketch_oct28a.ino
+++ sketch_oct28a.ino
@@ -20,3 +20,32 @@ int Moisture = 1; // initial value just in case web page is loaded before readMo
 int sensorVCC = 13;
 int blueLED = 2;
 DHT dht(DHTPin, DHTTYPE);   // Initialize DHT sensor.
+
+/*
+**** please enter your sensitive data in the Secret tab/arduino_secrets.h
+**** using format below
+
+#define SECRET_SSID "ssid name"
+#define SECRET_PASS "ssid password"
+#define SECRET_MQTTUSER "user name - eg student"
+#define SECRET_MQTTPASS "password";
+ */
+#define SECRET_SSID "CE-Hub-Student"
+#define SECRET_PASS "casa-ce-gagarin-public-service"
+#define SECRET_MQTTUSER "student"
+#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
+const char* ssid     = SECRET_SSID;
+const char* password = SECRET_PASS;
+const char* mqttuser = SECRET_MQTTUSER;
+const char* mqttpass = SECRET_MQTTPASS;
+
+ESP8266WebServer server(80);
+const char* mqtt_server = "mqtt.cetools.org";
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// Date and time
+Timezone GB;
