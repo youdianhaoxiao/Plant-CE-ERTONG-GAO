ba20173ff53d5ed2705807836e92f57e98e5d32c
youdianhaoxiao <146217421+youdianhaoxiao@users.noreply.github.com>
2023-10-30 18:46:24 +0000

    Update sketch_oct30a.ino

--- sketch_oct30a.ino
+++ sketch_oct30a.ino
@@ -177,3 +177,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 
 }
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    
+    // Attempt to connect with clientID, username and password
+    if (client.connect(clientId.c_str(), mqttuser, mqttpass)) {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe("student/CASA0014/plant/ucfnega/inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
