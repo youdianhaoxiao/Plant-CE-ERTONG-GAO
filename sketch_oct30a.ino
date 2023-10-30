9731dade1d2f6e5bca103a73cc41db4256571f39
youdianhaoxiao <146217421+youdianhaoxiao@users.noreply.github.com>
2023-10-30 17:54:38 +0000

    Update sketch_oct30a.ino

--- sketch_oct30a.ino
+++ sketch_oct30a.ino
@@ -1,1 +1,8 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ezTime.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
