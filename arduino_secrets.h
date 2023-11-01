7f429f7ee7eade091dc2acc4d852372f079a12cc
youdianhaoxiao <146217421+youdianhaoxiao@users.noreply.github.com>
2023-11-01 01:13:06 +0000

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -2,3 +2,4 @@
 #define SECRET_PASS "casa-ce-gagarin-public-service"
 #define SECRET_MQTTUSER "student"
 #define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
+Set WiFi and MQTT parameters: Get the SSID and password of the WiFi network and the username and password of the MQTT server from the external "arduino_secrets.h" file.
